6807d80c4f603f1e91891eff36b907e7437e8879
Patryk Chelmecki <pat.chelmecki@gmail.com>
2018-06-04 16:08:31 +0200

    checking if seat exists in plane

--- triggers.sql
+++ triggers.sql
@@ -3,8 +3,16 @@ $$
 declare
     a numeric;
 BEGIN
+    select count(*) into a from loty left join samoloty on loty.id_samolotu=samoloty.id_samolotu left join modele_samolotow on samoloty.id_modelu=modele_samolotow.model left join
+    miejsca_w_samolocie on miejsca_w_samolocie.id_modelu_samolot=modele_samolotow.model where loty.id_lotu=new.id_lotu;
+    case when a=0 then raise exception 'Nie ma takiego miejsca w samolocie';
+    end case;
+
     select count(*) into a from bilety where bilety.id_lotu=new.id_lotu and bilety.miejsce=new.miejsce;
-    case when a>0 then raise exception 'Ten bilet jest już sprzedany'; else return new; end case;
+    case when a>0 then raise exception 'Ten bilet jest już sprzedany';
+    end case;
+    
+    return new;
 END
 $$ LANGUAGE plpgsql;
 
