009949d9ba29b93033f73ab6e7bf9cf025c8b2a6
Patryk Chelmecki <pat.chelmecki@gmail.com>
2018-06-09 15:09:10 +0200

    bugfixes miejsce_check trigger

--- triggers.sql
+++ triggers.sql
@@ -4,14 +4,14 @@ declare
     a numeric;
 BEGIN
     select count(*) into a from loty left join samoloty on loty.id_samolotu=samoloty.id_samolotu left join modele_samolotow on samoloty.id_modelu=modele_samolotow.model left join
-    miejsca_w_samolocie on miejsca_w_samolocie.id_modelu_samolot=modele_samolotow.model where loty.id_lotu=new.id_lotu;
-    case when a=0 then raise exception 'Nie ma takiego miejsca w samolocie';
+    miejsca_w_samolocie on miejsca_w_samolocie.id_modelu_samolotu=modele_samolotow.model where loty.id_lotu=new.id_lotu and miejsca_w_samolocie.nr_miejsca=new.miejsce;
+    case when a=0 then raise exception 'Nie ma takiego miejsca w samolocie'; else
     end case;
 
     select count(*) into a from bilety where bilety.id_lotu=new.id_lotu and bilety.miejsce=new.miejsce;
-    case when a>0 then raise exception 'Ten bilet jest już sprzedany';
+    case when a>0 then raise exception 'Ten bilet jest już sprzedany'; else
     end case;
-    
+
     return new;
 END
 $$ LANGUAGE plpgsql;
